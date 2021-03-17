f3a2d05d163f2de1d3bb02aeb4849c9143e41c1c
nader-naderi <73893373+nader-naderi@users.noreply.github.com>
2021-03-17 15:45:47 +0330

    init commit

---
+++ Assets/_Scripts/Cell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NDRCellularAutomata
+{
+    public class Cell : MonoBehaviour
+    {
+        public bool isAlive = false;
+        public int numNeighbors = 0;
+
+        public void SetAlive(bool alive)
+        {
+            isAlive = alive;
+
+            if (alive)
+            {
+                GetComponent<SpriteRenderer>().enabled = true;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Game.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace NDRCellularAutomata
+{
+    public class Game : MonoBehaviour
+    {
+        public static Game instance;
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private static int SCREEN_WIDTH = 64; // 1024
+        private static int SCREEN_HEIGHT = 48; // 728
+
+        [SerializeField] float speed = 0.1f;
+
+        Cell[,] grid = new Cell[SCREEN_WIDTH, SCREEN_HEIGHT];
+
+        private float timer = 0;
+
+        private void Start()
+        {
+            PlaceCells();
+        }
+
+        private void Update()
+        {
+            if (timer >= speed)
+            {
+                CountNeighbors();
+                PopulationControl();
+                timer = 0;
+            }
+            else
+                timer += Time.deltaTime;
+        }
+
+
+        void PlaceCells()
+        {
+            for (int y = 0; y < SCREEN_HEIGHT; y++)
+            {
+                for (int x = 0; x < SCREEN_WIDTH; x++)
+                {
+                    Cell cell = Instantiate(Resources.Load("Prefabs/Cell", typeof(Cell)), new Vector2(x, y), Quaternion.identity) as Cell;
+                    grid[x, y] = cell;
+                    grid[x, y].SetAlive(GetRandomAliveCell());
+                }
+            }
+        }
+
+        bool GetRandomAliveCell()
+        {
+            int rand = UnityEngine.Random.Range(0, 100);
+            if (rand > 75)
+                return true;
+
+            return false;
+        }
+
+        void CountNeighbors()
+        {
+            for (int y = 0; y < SCREEN_HEIGHT; y++)
+            {
+                for (int x = 0; x < SCREEN_WIDTH; x++)
+                {
+                    int numNeighbors = 0;
+
+                    //- North
+                    if(y + 1 < SCREEN_HEIGHT)
+                        if (grid[x, y + 1].isAlive)
+                            numNeighbors++;
+
+                    //- East
+                    if(x+1 < SCREEN_WIDTH)
+                        if (grid[x + 1, y].isAlive)
+                            numNeighbors++;
+
+                    //- South
+                    if (y - 1 >= 0)
+                        if (grid[x, y - 1].isAlive)
+                            numNeighbors++;
+
+                    //- West
+                    if (x - 1 >= 0)
+                        if (grid[x - 1, y].isAlive)
+                            numNeighbors++;
+
+                    //- North East
+                    if (x + 1 < SCREEN_WIDTH && y + 1 < SCREEN_HEIGHT)
+                        if (grid[x + 1, y + 1].isAlive)
+                            numNeighbors++;
+
+                    //- North West
+                    if (x - 1 >= 0 && y + 1 < SCREEN_HEIGHT)
+                        if (grid[x - 1, y + 1].isAlive)
+                            numNeighbors++;
+
+                    //- South East
+                    if (x + 1 < SCREEN_WIDTH && y - 1 >= 0)
+                        if (grid[x + 1, y - 1].isAlive)
+                            numNeighbors++;
+
+                    //- South West
+                    if (x - 1 >= 0 && y - 1 >= 0)
+                        if (grid[x - 1, y - 1].isAlive)
+                            numNeighbors++;
+
+
+                    grid[x, y].numNeighbors = numNeighbors;
+                }
+            }
+        }
+        void PopulationControl()
+        {
+            for (int y = 0; y < SCREEN_HEIGHT; y++)
+            {
+                for (int x = 0; x < SCREEN_WIDTH; x++)
+                {
+                    //- Rules.
+                    // Any Live Cell with two or three live neighbors, survives.
+                    // Any dead cell with 3 live neighbors, becomes alive.
+                    // All other live cells die in the next Generation and all dead cells stay dead.
+
+                    if(grid[x, y].isAlive)
+                    {
+                        // Cell is Alive.
+                        if(grid[x, y].numNeighbors != 2 && grid[x, y].numNeighbors != 3)
+                        {
+                            grid[x, y].SetAlive(false);
+                        }
+                    }
+                    else
+                    {
+                        // Cell is dead.
+                        if(grid[x, y].numNeighbors == 3)
+                        {
+                            grid[x, y].SetAlive(true);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+   
+}
